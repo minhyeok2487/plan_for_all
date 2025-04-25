e732add5cf34cef5eb31ec19a7a70a63ebdccc6b
Minhyeok1995 <repeater2487@naver.com>
2025-04-25 11:10:01 +0900

    fix: 처음 킬때 로그인 여부 파악 수정
    - README 수정

--- lib/app.dart
+++ lib/app.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:plan_for_all/providers/user_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
+import 'providers/user_provider.dart';
+import 'services/task_service.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final session = Supabase.instance.client.auth.currentSession;
-
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()..loadUser()), // ✅ 유저 정보 불러오기
+        ChangeNotifierProvider(create: (_) => TaskService(Supabase.instance.client)),
       ],
-      child: MaterialApp(
-        title: 'Plan For All',
-        theme: ThemeData.dark(),
-        debugShowCheckedModeBanner: false,
-        home: session == null ? const LoginScreen() : const HomeScreen(),
-      ),
+      child: const _AppRouter(),
+    );
+  }
+}
+
+class _AppRouter extends StatelessWidget {
+  const _AppRouter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = context.watch<UserProvider>().user;
+
+    return MaterialApp(
+      title: 'Plan For All',
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: user == null ? const LoginScreen() : const HomeScreen(),
     );
   }
 }
